7a5d99bec5e9417931a6a22827a94a95cbd8f3d5
saumya <jsaumya988@gmail.com>
2020-09-06 18:32:56 +0530

    trying 1

--- src/Components/FaceBookLoginButton.js
+++ src/Components/FaceBookLoginButton.js
@@ -8,7 +8,7 @@ export const FacebookLoginButton = () => {
     const [isAuth, setAuth] = React.useState(false);
     return (
     <div>
-        <div onClick={() => {FacebookLogin(isAuth)}} className="fb-login-button" data-size="large" data-button-type="continue_with" data-layout="default"
+        <div onClick={FacebookLogin} className="fb-login-button" data-size="large" data-button-type="continue_with" data-layout="default"
              data-auto-logout-link="false" data-use-continue-as="false" data-width=""/>
         {isAuth && <Link to="/user" />}
     </div>
--- src/Login/FacebookLogin.js
+++ src/Login/FacebookLogin.js
@@ -5,7 +5,6 @@ export const FacebookLogin = (props) =>{
         console.log('Welcome!  Fetching your information.... ');
         window.FB.api('/me', function(response) {
           console.log('Good to see you, ' + response.name + '.');
-          props.auth = true;
         });
       } else {
         console.log('User cancelled login or did not fully authorize.');
